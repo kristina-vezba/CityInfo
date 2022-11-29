651277a29474145e0410d27c134da53aadc16051
kristina-vezba <4kristinabegovic@gmail.com>
2022-11-29 15:44:53 +0100

    Add project files.

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<PointOfInterestDto> GetPointsOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointsOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+            if(pointOfInterestId == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointsOfInterest);
+        }
+    }
+}
